8e6cb5e2907cbea5ba0aa27fbe7289e6745dab51
Konrad Karczewski <kkarczewski94@gmail.com>
2020-09-26 20:21:24 +0200

    Styling of important tasks and showing of date

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,35 @@
 import React from "react";
 
 const Task = (props) => {
-  const { text, date, id } = props.task;
+  const importantStyle = {
+    color: "red",
+  };
+
   const { changeStatusHandler, deleteTaskHandler } = props;
-  return (
-    <div>
-      <strong>{text}</strong> - do <span>{date} </span>
-      <button onClick={() => changeStatusHandler(id)}>Zostało zrobione</button>
-      <button onClick={() => deleteTaskHandler(id)}>X</button>
-    </div>
-  );
+  const { text, date, id, isActive, isImportant, finishDate } = props.task;
+
+  if (isActive) {
+    return (
+      <div>
+        <strong style={isImportant ? importantStyle : null}>{text}</strong> - do{" "}
+        <span>{date} </span>
+        <button onClick={() => changeStatusHandler(id)}>
+          Zostało zrobione
+        </button>
+        <button onClick={() => deleteTaskHandler(id)}>X</button>
+      </div>
+    );
+  } else {
+    const finishTime = new Date(finishDate).toLocaleString();
+    return (
+      <div>
+        <strong>{text}</strong>
+        <em> (zrobić do {date}) </em>
+        <br />- potiwerdzenie wykonania <span>{finishTime}</span>
+        <button onClick={() => deleteTaskHandler(id)}>X</button>
+      </div>
+    );
+  }
 };
 
 export default Task;

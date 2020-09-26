06a4d9e19ac63698368bc4a010e16575c6de74ec
Konrad Karczewski <kkarczewski94@gmail.com>
2020-09-26 20:55:04 +0200

    Added header tag

--- src/components/App.js
+++ src/components/App.js
@@ -105,7 +105,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        TODO APP
+        <h1>TODO APP</h1>
         <AddTask />
         <TaskList
           tasks={this.state.tasks}

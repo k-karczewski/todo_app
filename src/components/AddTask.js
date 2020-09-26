4d8e20f7af026ef54886bea8c72c675414e97e4b
Konrad Karczewski <kkarczewski94@gmail.com>
2020-09-26 20:54:45 +0200

    First version of AddTask form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./AddTask.css";
 
 class AddTask extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>Dodaj task</div>
-         );
-    }
+  state = {
+    text: "",
+    checked: false,
+    date: "2020-01-25",
+  };
+
+  render() {
+    return (
+      <div className="form">
+        <input
+          type="text"
+          placeholder="dodaj zadanie"
+          value={this.state.text}
+        />
+
+        <input id="important" type="checkbox" checked={this.state.checked} />
+        <label htmlFor="important">Priorytet </label>
+        <br />
+
+        <label htmlFor="date">Do kiedy zrobić</label>
+        <input
+          id="date"
+          type="date"
+          value={this.state.date}
+          min="2020-01-01"
+          max="2022-12-31"
+        />
+        <br />
+        <button>Dodaj</button>
+      </div>
+    );
+  }
 }
- 
+
 export default AddTask;

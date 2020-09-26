f42d0e98ca379f68142c34bdd651bdcde159c9ec
Konrad Karczewski <kkarczewski94@gmail.com>
2020-09-26 20:01:50 +0200

    Sorting of active and done tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,11 @@ import Task from "./Task";
 
 const TaskList = (props) => {
   const { deleteTaskHandler, changeStatusHandler } = props;
-  const tasks = props.tasks.map((task) => (
+
+  const active = props.tasks.filter((task) => task.isActive);
+  const done = props.tasks.filter((task) => !task.isActive);
+
+  const activeTasks = active.map((task) => (
     <Task
       key={task.id}
       task={task}
@@ -11,17 +15,37 @@ const TaskList = (props) => {
       changeStatusHandler={changeStatusHandler}
     />
   ));
+
+  const doneTasks = done.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      deleteTaskHandler={deleteTaskHandler}
+      changeStatusHandler={changeStatusHandler}
+    />
+  ));
+
   return (
     <Fragment>
       <div className="active">
         <h1>Zadania do zrobienia</h1>
-        {tasks}
+        {activeTasks.length > 0 ? (
+          activeTasks
+        ) : (
+          <p>nie masz nic do roboty :)</p>
+        )}
       </div>
 
       <hr />
 
       <div className="done">
-        <h3>Zadania zrobione (0)</h3>
+        <h3>
+          Zadania zrobione <em>({doneTasks.length})</em>
+        </h3>
+        {done.length > 5 && (
+          <span style={{fontSize: 10}}>Wyświetlonych jest jedynie 5 ostatnich zadań</span>
+        )}
+        {doneTasks.slice(0, 5)}
       </div>
     </Fragment>
   );

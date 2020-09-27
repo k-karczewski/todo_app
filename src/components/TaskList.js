eac846a8d33664e95f3be00f784abcce37038bba
Konrad Karczewski <kkarczewski94@gmail.com>
2020-09-27 11:17:15 +0200

    Sorting of tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,6 +7,21 @@ const TaskList = (props) => {
   const active = props.tasks.filter((task) => task.isActive);
   const done = props.tasks.filter((task) => !task.isActive);
 
+  if (done.length >= 2) {
+    done.sort((a, b) => b.finishDate - a.finishDate);
+  }
+
+  if (active.length >= 2) {
+    active.sort((a, b) => {
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+
+      if (a < b) return -1;
+      else if (a > b) return 1;
+      else return 0;
+    });
+  }
+
   const activeTasks = active.map((task) => (
     <Task
       key={task.id}
@@ -43,7 +58,9 @@ const TaskList = (props) => {
           Zadania zrobione <em>({doneTasks.length})</em>
         </h3>
         {done.length > 5 && (
-          <span style={{fontSize: 10}}>Wyświetlonych jest jedynie 5 ostatnich zadań</span>
+          <span style={{ fontSize: 10 }}>
+            Wyświetlonych jest jedynie 5 ostatnich zadań
+          </span>
         )}
         {doneTasks.slice(0, 5)}
       </div>
